9d4199553e4710c1ce605eaa5473e237b37a3ab9
PrivateAndr <ax308ak103@gmail.com>
2022-11-10 20:49:08 +0200

    Start of the project

---
+++ shop/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from decimal import Decimal
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+class Plant(models.Model):
+    category = models.ForeignKey(Category, related_name='plants', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, primary_key=True)
+    price = models.DecimalField(max_digits=20, decimal_places=1, default=Decimal(0.00))
+    size = models.DecimalField(max_digits=20, decimal_places=1, default=Decimal(0.00))
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    storage = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, db_index=True)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('name',)
+        index_together = (('slug'),)
+
+    def __str__(self):
+        return self.name
